5c54d1b0e89af8a4e6dc00e3943d9942fc225726
Vguffin <vguffin.prsvr@gmail.com>
2024-02-27 12:59:06 -0600

    updated files

--- Assignments/Week 5/playing-with-strings/main.js
+++ Assignments/Week 5/playing-with-strings/main.js
@@ -1,6 +1,99 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+`xify` - returns the same string, but with every character replaced by an 'x'
+    * xify('hi') -> 'xx'
+    * xify('hello') -> 'xxxxx'
+    
+
+* `yellingChars` - returns the given string with an exclamation point after each character
+ 
+    * yellingChars('gracious') -> 'g!r!a!c!i!o!u!s!'
+    * yellingChars('oh hi') -> 'o!h! h!i!'
+  * Hints:
+    * We can add more than one thing to the string each time through the loop. In this case, it's the current character AND an exlamation point.
+
+
+* `indexedChars` - adds the index of each character before that character in the given string
+  * Examples:
+    * indexedChars('hey') -> '0h1e2y'
+    * indexedChars('byebye') -> '0b1y2e3b4y5e'
+  * Hints: 
+    * We can add something BEFORE the current character as well!
+
+
+* `numberedChars` - adds the number of each character before that character in the given string
+  * Examples:
+    * numberedChars('hey') -> '(1)h(2)e(3)y'
+    * numberedChars('byebye') -> '(1)b(2)y(3)e(4)b(5)y(6)e'
+  * Hints: 
+    * there's a simple relationship between the index and the number... indices are 0-based counting and numbers are 1-based... you'll figure it out!
+
+
+* `exclaim` - returns the given sentence with every question mark or period changed to an exclamation point
+  * Examples:
+    * exclaim('How are you doing? Are you a fool?') -> 'How are you doing'
+    * exclaim('This is not fine.') -> 'This is not fine!'
+
+
+* `repeatIt` - returns the given string repeated `n` times, where `n` is the second parameter
+  * Examples:
+    * repeatIt('three', 3) -> 'threethreethree'
+    * repeatIt('oh hi!', 7) -> 'oh hi!oh hi!oh hi!oh hi!oh hi!oh hi!oh hi!'
+
+
+* `truncate` - shortens a long string to 15 characters plus an ellipsis (...)
+  * Examples:
+    * truncate('The fault, dear Brutus, is not in our stars, but in ourselves.') -> 'The fault, dear...'
+    * truncate("Well, that's just, like, your opinion man.") -> "Well, that's ju..."
+
+
+* `ciEmailify` - creates an email from a two-part name
+  * Examples:
+    * ciEmailify('victoria guffin') -> 'victoria.guffin@codeimmersives.com'
+    * ciEmailify('Christy Marion') -> 'christy.marion@codeimmersives.com'
+
+
+* `reverse` - reverses the given string
+  * Examples:
+    * reverse('hannah') -> 'hannah'
+    * reverse('Aya') -> 'Aya'
+    
+
+* `onlyVowels` - returns only the vowels from a word
+  * Examples:
+    * onlyVowels('Aya') -> 'aa'
+    * onlyVowels('Victoria') -> 'ioia'
+    * onlyVowels('Anthony') -> 'Aoy'
+
+## Extra stretch goals
+
+* `crazyCase` - returns the given string with alternating lower and upper cases
+  * Examples:
+    * crazyCase('hey') -> 'hEy'
+    * crazyCase('multiple words') -> 'mUlTiPlE WoRdS'
+    * crazyCase('YELL') -> 'yElL'
+     
+    
+* `titleCase` - returns a transformed version of the given string where every word starts with a capital letter and every non-word-starting letter is lowercased
+  * Examples:
+    * titleCase('the lion king') -> 'The Lion King'
+    * titleCase('cOde iMMerSives') -> 'Code Immersives'
+     
+   
+    
+* `camelCase` - returns the given string in camel case
+  * Examples:
+    * camelCase('oh Hi') -> 'ohHi'
+    * camelCase('well yeah') -> 'wellYeah'
+    * camelCase(' howdy there') -> 'HowdyThere'
+    * 
+    
+    
+* `crazyCase2ReturnOfCrazyCase` - same as `crazyCase`, but does NOT count spaces as letters to upper or lower case (see examples below!)
+  * Examples:
+    * crazyCase2ReturnOfCrazyCase('multiple words') -> 'mUlTiPlE wOrDs'
+    * crazyCase2ReturnOfCrazyCase('crazy stuff here') -> 'cRaZy StUfF hErE'
 
 
 
--- Assignments/Week 5/while-e-coyote/main.js
+++ Assignments/Week 5/while-e-coyote/main.js
@@ -1,17 +1,20 @@
 function numberDoubler(num) {
-    // Your code here
+    numberDoubler(51)  => 102
+numberDoubler(100) => 100
+numberDoubler(26)  => 104
 }
 
 function stringRepeater(str) {
-    // Your code here
+    stringRepeater('abc')    => 'abcabcabcabc'
+stringRepeater('a')      => 'aaaaaaaaaa'
+stringRepeater('hotdog') => 'hotdoghotdog'
 }
 
 function makeDivisible(x, y) {
-    // Your code here
+    makeDivisible(9, 1)  => 9
+makeDivisible(9, 2)  => 10
+makeDivisible(11, 3) => 12
+makeDivisible(10, 3) => 12
+makeDivisible(5, 4)  => 8
+makeDivisible(16, 5) => 20
 }
-
-module.exports = {
-    numberDoubler,
-    stringRepeater,
-    makeDivisible
-};
